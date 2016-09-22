a375bf95881b4da7797fd993393312484ddaccaa
Rick Garcia <outcastsoftware@gmail.com>
2016-09-21 18:27:54 -0700

    Convert to Android

---
+++ Assets/Player/ReplaySystem.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReplaySystem : MonoBehaviour {
+
+	private const int bufferFrames = 1000;
+	private MyKeyFrame[] keyFrames = new MyKeyFrame [bufferFrames];
+	private Rigidbody rigidBody;
+	private GameManager manager;
+
+
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		manager = GameObject.FindObjectOfType<GameManager>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (manager.recording) {
+			Record ();
+		} else {
+			PlayBack();
+		}
+	}
+
+	void PlayBack () {
+		rigidBody.isKinematic = true;
+		int frame = Time.frameCount % bufferFrames;
+		transform.position = keyFrames[frame].position;
+		transform.rotation = keyFrames[frame].rotation;
+
+	}
+
+	void Record ()
+	{
+		rigidBody.isKinematic = false;
+		int frame = Time.frameCount % bufferFrames;
+		float time = Time.time;
+		keyFrames [frame] = new MyKeyFrame (time, transform.position, transform.rotation);
+//		Debug.Log ("Recording: " + frame);
+	}
+}
+
+/*public struct MyKeyFrame {
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation) {
+		frameTime = aTime;
+		position = aPosition;
+		rotation = aRotation;
+	}
+}
+*/
+
+public struct MyKeyFrame {
+
+	public float frameTime;
+	public Vector3 position;
+	public Quaternion rotation;
+
+	public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation) {
+		frameTime = aTime;
+		position = aPosition;
+		rotation = aRotation;
+	}
+}
+
+
+//public class MyKeyFrame {
+//
+//	public float frameTime;
+//	public Vector3 position;
+//	public Quaternion rotation;
+//
+//	public MyKeyFrame (float aTime, Vector3 aPosition, Quaternion aRotation) {
+//		frameTime = aTime;
+//		position = aPosition;
+//		rotation = aRotation;
+//
+//	}
+//
+//}
